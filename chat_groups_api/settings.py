c5c6703ed268c81bea16177bd527413bc7ac1555
Rodrigo García <yo@rodrigogarcia.me>
2022-06-17 19:12:20 +0200

    Add PostgreSQL

--- chat_groups_api/settings.py
+++ chat_groups_api/settings.py
@@ -80,9 +80,15 @@ ASGI_APPLICATION = 'chat_groups_api.asgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'chat-groups',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'chat-groups-api',
+        'PASSWORD': 'monopatin'
     }
 }
 
